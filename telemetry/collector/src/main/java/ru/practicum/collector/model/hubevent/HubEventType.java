eba4b72305aa59d3600188da0480706b0d0341fc
Илья <iljiafenix@yandex.ru>
2025-11-16 15:36:39 +0300

    fix:

--- telemetry/collector/src/main/java/ru/practicum/collector/model/hubevent/HubEventType.java
+++ telemetry/collector/src/main/java/ru/practicum/collector/model/hubevent/HubEventType.java
@@ -2,7 +2,7 @@ package ru.practicum.collector.model.hubevent;
 
 public enum HubEventType {
     DEVICE_ADDED,
-    DEVICE_REMOVED_EVENT,
-    SCENARIO_ADDED_EVENT,
-    SCENARIO_REMOVED_EVENT,
+    DEVICE_REMOVED,
+    SCENARIO_ADDED,
+    SCENARIO_REMOVED,
 }
