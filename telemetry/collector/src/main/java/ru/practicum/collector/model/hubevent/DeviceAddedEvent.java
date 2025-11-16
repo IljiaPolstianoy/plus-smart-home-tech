1079e10d8f883b56c01411818d43b69072973796
Илья <iljiafenix@yandex.ru>
2025-11-16 15:31:00 +0300

    fix:

--- telemetry/collector/src/main/java/ru/practicum/collector/model/hubevent/DeviceAddedEvent.java
+++ telemetry/collector/src/main/java/ru/practicum/collector/model/hubevent/DeviceAddedEvent.java
@@ -1,6 +1,7 @@
 package ru.practicum.collector.model.hubevent;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -13,7 +14,7 @@ public class DeviceAddedEvent extends HubEvent {
     @NotBlank
     private String id;
 
-    @NotBlank
+    @NotNull
     private DeviceType deviceType;
 
     @Override
